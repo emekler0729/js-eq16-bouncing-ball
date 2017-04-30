b1727db5fd89486e9ec91c74b3fe710768bb4e2f
Eduard Mekler <emekler0729@gmail.com>
2017-04-29 21:38:31 -0500

    Added crude collision detection. Currently issue exists where objects aren't prevented from penetrating each other which causes odd behavior.

--- code/ball.js
+++ code/ball.js
@@ -2,27 +2,58 @@
  * Created by Eduard on 4/29/2017.
  */
 
-function Level(width, height, actors) {
+function Level(width, height, objects) {
     this.width = width;
     this.height = height;
-    this.actors = actors;
+    this.objects = objects;
 }
 
-// @TODO level collision detection
-Level.prototype.obstacleAt = function() {
+Level.prototype.detectCollision = function(object) {
+    var xCollision, yCollision = false;
 
-};
+    if(object.x - object.r < 0 || object.x + object.r >= this.width) {
+        xCollision = true;
+    }
 
-// @TODO object collision detection
-Level.prototype.objectAt = function() {
+    if(object.y - object.r < 0 || object.y + object.r >= this.height) {
+        yCollision = true;
+    }
 
+    this.objects.forEach(function(other) {
+        if(other != object) {
+            var dx = Math.abs(object.x - other.x);
+            var dy = Math.abs(object.y - other.y);
+            var distance = Math.sqrt(dx * dx + dy * dy);
+            if(distance < object.r + other.r) {
+                var angle = Math.tan(dy/dx);
+
+                if(angle >= 7/4*Math.PI && angle < Math.PI/4 || angle >= 3/4*Math.PI && angle < 5/4*Math.PI) {
+                    xCollision = true;
+                    other.xSpeed = -other.xSpeed;
+                } else {
+                    yCollision = true;
+                    other.ySpeed = -other.ySpeed;
+                }
+            }
+        }
+    });
+
+    if(xCollision && yCollision) {
+        return "xy";
+    } else if (xCollision) {
+        return 'x';
+    } else if (yCollision) {
+        return 'y';
+    } else {
+        return null;
+    }
 };
 
 Level.prototype.animate = function(step) {
     while(step > 0) {
         var thisStep = Math.min(step, 0.05);
-        this.actors.forEach(function(actor) {
-            actor.act(thisStep, this);
+        this.objects.forEach(function(object) {
+            object.act(thisStep, this);
         }, this);
         step -= thisStep;
     }
@@ -39,6 +70,21 @@ function Ball(x, y, r, xSpeed, ySpeed) {
 Ball.prototype.act = function(timeStep, level) {
     this.x = this.x + (this.xSpeed * timeStep);
     this.y = this.y + (this.ySpeed * timeStep);
+
+    var collision = level.detectCollision(this);
+
+    switch(collision) {
+        case "xy":
+            this.xSpeed = -this.xSpeed;
+            this.ySpeed = -this.ySpeed;
+            break;
+        case 'x':
+            this.xSpeed = -this.xSpeed;
+            break;
+        case 'y':
+            this.ySpeed = -this.ySpeed;
+            break;
+    }
 };
 
 Ball.prototype.draw = function(context) {
@@ -78,7 +124,7 @@ CanvasDisplay.prototype.drawBackground = function() {
 };
 
 CanvasDisplay.prototype.drawObjects = function() {
-    this.level.actors.forEach(function(object) {
+    this.level.objects.forEach(function(object) {
         object.draw(this.context);
     }, this)
 };
